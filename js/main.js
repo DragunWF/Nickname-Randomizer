707b5829b2c216915385c6b9569088994175dd5f
dragunwf <plarisanmarc@gmail.com>
2023-10-15 14:03:34 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
+import Preset from "./preset.js";
+
 const outputText = document.getElementById("output");
+const presets = [
+  new Preset("Dragon's Castle - DragunWF", [], []),
+  new Preset(
+    "Dragon's Castle - Thrawn",
+    ["Jewker", "CPT", "Dragun", "Faith", "Thrawn", "Mortis"],
+    ["The Slut", "The Pro", "The Horny", "The Brave", "The Supergay"]
+  ),
+];
 
 function giveName() {
   const firstNameArr = ["Jewker", "CPT", "Dragun", "Faith", "Thrawn", "Mortis"];
@@ -12,6 +22,19 @@ function giveName() {
   outputText.innerText = `${getRandomItem(firstNameArr)} ${getRandomItem(
     lastNameArr
   )}`;
+}
+
+function addFirstName() {
+  addName(true);
+}
+
+function addLastName() {
+  lastName(false);
+}
+
+function addName(isFirstName) {
+  const name = document.getElementById(isFirstName ? "firstName" : "lastName");
+  // add logic
 }
 
 function getRandomItem(arr) {
