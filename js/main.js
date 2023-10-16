731edda82cbee143509a5d61f77b585abe536034
dragunwf <plarisanmarc@gmail.com>
2023-10-16 18:13:57 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,13 @@ const presets = [
     ["The Slut", "The Pro", "The Horny", "The Brave", "The Supergay"]
   ),
 ];
+const interfaces = {
+  presetUI: document.getElementById("presetUI"),
+  mainUI: document.getElementById("mainUI"),
+};
+
 let currentPreset = new Preset();
+let presetUIVisible = true; // Always set to false unless testing
 
 function giveName() {
   const firstNameArr = ["Jewker", "CPT", "Dragun", "Faith", "Thrawn", "Mortis"];
@@ -40,16 +46,29 @@ function getRandomItem(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function addFirstName() {
-  addName(true);
-}
-
-function addLastName() {
-  lastName(false);
-}
-
 function addPreset() {}
 
 function resetPreset() {
   let currentPreset = new Preset();
 }
+
+window.addFirstName = () => addName(true);
+window.addLastName = () => addName(false);
+window.togglePresetUI = () => {
+  const presetUIClasses = interfaces.presetUI.getAttribute("class").split(" ");
+  const mainUIClasses = interfaces.mainUI.getAttribute("class").split(" ");
+
+  presetUIVisible = !presetUIVisible;
+  if (presetUIVisible) {
+    presetUIClasses.pop(); // last element will always be "hide"
+    mainUIClasses.push("hide");
+  } else {
+    mainUIClasses.pop();
+    presetUIClasses.push("hide");
+  }
+
+  interfaces.presetUI.setAttribute("class", presetUIClasses.join(" "));
+  interfaces.mainUI.setAttribute("class", mainUIClasses.join(" "));
+};
+
+console.log("main.js has been loaded!");
